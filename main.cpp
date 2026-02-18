f7f69019f176308e5b5732cf01e0c4c2016ac6f8
hklt502 <hokimluuthong007@gmail.com>
2026-02-18 09:30:12 +0700

    trang tri

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <conio.h>
 #include <cstdlib>
+#include <cctype>
 #include <windows.h>
-#include <cstdlib>
-#include <fstream>
+#include <string>
 #include <ctime>
 using namespace std;
 
@@ -30,7 +30,6 @@ void colorRed();
 void colorBlue();
 void colorGreen();
 void colorYellow();
-void colorOrange();
 void colorDefault();
 
 //countdown
@@ -41,7 +40,7 @@ void computerThinking();
 void printTwoIcons(int a, int b);
 
 int main() {
-    srand(time(NULL)); // seed cho rand()
+    srand(time(NULL));// initialize the seed
     menu();
     return 0;
 }
@@ -50,23 +49,25 @@ void menu() {
     char choice;
     do {
         colorBlue();
-        cout<< "                     | __     __    _________      (\\   /) |\n";
-        cout<< "                     | ||     ||   //00O0O00O\\\      (\\ /)  |\n";
-        cout<< "                     | |(@@@@)||  //09O00O09O0\\\      (X)   |\n";
-        cout<< "                     | (@@@@@@@)  \\9080O600O0O//     /_\\   |\n";
-        cout<< "                     |(@@@@@@@@@)  \\9080OO809//     /| |\\  |\n";
-        cout<< "                     | (@@@@@@@)    \\9080OO80/     /_| |_\\ |\n";
+        cout << "  +----------------------------------------+\n";
+        cout << "  | __   __     _________       (\\   /)    |\n";
+        cout << "  | ||   ||    //00O0O00O\\\\      (\\ /)     |\n";
+        cout << "  | |(@@)||    //09O00O09O0\\\\     (X)      |\n";
+        cout << "  |(@@@@@@)    \\\\9080O600O0O//    /_\\      |\n";
+        cout << "  |(@@@@@@@)    \\\\9080OO809//    /| |\\     |\n";
+        cout << "  | (@@@@@@)     \\\\9080OO80/    /_| |_\\    |\n";
+        cout << "  +----------------------------------------+\n";
         colorRed();
-        cout<< "\n                        | ===== ROCK PAPER SCISSORS =====|\n";
+        cout << "  |        * ROCK  PAPER  SCISSORS *       |\n";
         colorGreen();
-        cout<< "\n                        | 1. Play with computer          |\n";
-        cout<< "\n                        | 2. Play with another player    |\n";
-        cout<< "\n                        | 0. Exit                        |\n";
-        cout<< "                         --------------------------------\n";
-        cout<< "\n                         Choose: ";
-
-        cin>> choice;
-        cout<< choice << endl;
+        cout << "  +----------------------------------------+\n";
+        cout << "  |    [1] Play with computer              |\n";
+        cout << "  |    [2] Play with another player        |\n";
+        cout << "  |    [0] Exit                            |\n";
+        cout << "  +----------------------------------------+\n";
+        colorYellow();
+        cout << "  Choose: ";
+        cin >> choice;
         colorDefault();
         if (choice == '1'){
             system("cls");
@@ -85,31 +86,38 @@ int getPlayerChoice() {
     char c;
     do {
         colorGreen();
-        cout<< "\n                               Choose player 1: ";
+        cout << "\n  +------------------+\n  | Choose player 1: ";
         colorDefault();
         c = _getch();
         if(c < '1' || c > '3'){
-            cout<< "\33[0A\33[0K\r";
+            cout << "\33[1A\33[2K\r";
+            cout << "\33[0A\33[2K\r";
         }
     } while (c < '1' || c > '3');
-
+    colorGreen();
+    cout<< " |\n";
+    cout<<"  +------------------+\n";
+    colorDefault();
     return c - '0';
 }
 
 int getPlayerChoice1() {
     char c;
-    cout<< "\n\n";
     do {
         colorRed();
-        cout<< "\n                               Choose player 2: ";
+        cout << "\n  +------------------+\n  | Choose player 2: ";
         colorDefault();
         c = _getch();
         if(c < '7' || c > '9'){
-            cout<< "\33[0A\33[0K\r";
+                cout << "\33[1A\33[2K\r";
+                cout << "\33[0A\33[2K\r";
         }
     } while (c < '7' || c > '9');
-
-    return c - '0' ;
+    colorRed();
+    cout<< " |\n";
+    cout<< "  +------------------+\n";
+    colorDefault();
+    return c - '0';
 }
 
 
@@ -138,55 +146,39 @@ void playWithPlayer() {
     string items[3] = {"Rock", "Paper", "Scissors"};
     do {
         colorYellow();
-        cout<< "                            ======================================\n";
+        cout<< "  +------------------------------------------+\n";
+        cout<< "  |         PLAY WITH ANOTHER PLAYER         |\n";
+        cout<< "  +------------------------------------------+\n";
         colorGreen();
-        cout<< "                            Player 1:(1.Rock  2.Paper  3.Scissors)\n";
-        colorYellow();
-        cout<< "                            **************************************\n";
+        cout<< "  | P1: [1]Rock [2]Paper [3]Scissors         |\n";
         colorRed();
-        cout<< "                            Player 2:(7.Rock  8.Paper  9.Scissors)\n";
-        colorYellow();
-        cout<< "                            ======================================\n";
+        cout<< "  | P2: [7]Rock [8]Paper [9]Scissors         |\n";
+        cout<< "  +------------------------------------------+\n";
         colorDefault();
-        cout<< "                                 (\\___/)            (\\___/)\n";
-        cout<< "                                 ( '- ')            (*- * )\n";
-        cout<< "                                 (\")__(\")          (\\____/)\n";
-
+        cout << "     (\\___/)              (\\___/)\n";
+        cout << "     ( '- ')              (*- * )\n";
+        cout << "     (\")__(\")            (\\____/)\n";
         int player1 = getPlayerChoice();
-
         int player2 = getPlayerChoice1();
-
-        int result = getResult(player1, player2 );
-
+        int result = getResult(player1, player2);
         countdown();
         colorGreen();
-        cout<< "\n\n                           "<< items[player1 - 1];
+        cout<< "\n    " << items[player1 - 1];
+        colorDefault();
+        cout<< "   vs   ";
         colorRed();
-        cout<< "              vs              "<< items[player2 - 7]<< endl<<endl;
+        cout<< items[player2 - 7] << endl << endl;
         colorDefault();
         printTwoIcons(player1, player2 - 6);
-        if (result == 0){
-            cout<< "\n                        ----------------------------------------------------\n\n";
-            colorYellow();
-            cout<< "                                            Result: Draw\n";
-            colorDefault();
-        }
-
-        else if (result == 1){
-            cout<< "\n                        ----------------------------------------------------\n\n";
-            colorGreen();
-            cout<< "                                        Result: Player 1 wins!\n";
-            colorDefault();
-        }
-        else{
-            cout<< "\n                        ----------------------------------------------------\n\n";
-            colorRed();
-            cout<< "                                        Result: Player 2 wins!\n";
-            colorDefault();
-        }
-
-        cout << "\n\n                         Press Y to exit, press any key to continue.";
-        exitKey = tolower(getch());
+        cout << "  +------------------------------------------+\n";
+        if (result == 0) { colorYellow(); cout << "  |           Result: Draw                    |\n"; }
+        else if (result == 1) { colorGreen(); cout << "  | Result: Player 1 wins!          |\n"; }
+        else { colorRed(); cout << "  | Result: Player 2 wins!          |\n"; }
+        colorDefault();
+        cout << "  +------------------------------------------+\n";
+        cout << "  |   Press Y to exit, any key to continue.  |\n";
+        cout << "  +------------------------------------------+\n";
+        exitKey = tolower(_getch());
         system("cls");
     } while (exitKey != 'y');
 }
@@ -194,58 +186,51 @@ void playWithPlayer() {
 void playWithComputer() {
     string items[3] = {"Rock", "Paper", "Scissors"};
     char exitKey;
-    do{
+    do {
         colorRed();
-        cout<< "                         ==========*PLAY_WITH_COMPUTER*========\n";
-        colorYellow();
-        cout<< "                         ======================================\n";
+        cout << "  +------------------------------------------+\n";
+        cout << "  |            PLAY WITH COMPUTER            |\n";
+        cout << "  +------------------------------------------+\n";
         colorGreen();
-        cout<< "                         Player 1:(1.Rock  2.Paper  3.Scissors)\n";
-        colorYellow();
-        cout<< "                         ======================================\n";
-        cout<< "                               (\\___/)             ___!___ \n";
-        cout<< "                               ( '- ')             [A~ I ] \n";
-        cout<< "                               (\")__(\")            />____<\\  \n\n";
+        cout << "  |       [1]Rock [2]Paper [3]Scissors       |\n";
+        cout << "  +------------------------------------------+\n";
+        colorDefault();
+        cout << "     (\\___/)              ___!___\n";
+        cout << "     ( '- ')              [A~ I ]\n";
+        cout << "     (\")__(\")             />____<\\\n";
         colorBlue();
-        cout<< "                         ________________SCORE_________________\n";
+        cout << "  +---------------- SCORE ----------------+\n";
         colorGreen();
-        cout<< "                              You: " << playerScore;
+        cout<< "  |     You: " << playerScore << "             ";
         colorRed();
-        cout<< "              Computer: " << computerScore<< endl;
+        cout<< "Computer: " << computerScore;
         colorBlue();
-        cout<< "                         *---*---*---*---*----*---*---*---*---*\n";
+        cout << "  |\n  +------------------------------------------+\n";
         colorDefault();
         int playerChoice = getPlayerChoice();
         int computerChoice = getComputerChoice();
         int result = getResult(playerChoice, computerChoice);
         updateScore(result);
         computerThinking();
-        cout<<endl;
-        cout<< "\n\n                           "<< items[playerChoice - 1]<< "              vs              "<< items[computerChoice - 7]<< endl<<endl;
+        colorGreen();
+        cout << "\n  " << items[playerChoice - 1];
+        colorDefault();
+        cout << "   vs   ";
+        colorRed();
+        cout<< items[computerChoice - 7] << endl << endl;
+        colorDefault();
         printTwoIcons(playerChoice, computerChoice - 6);
-
-
-        if (result == 0){
-            colorYellow();
-            cout<< "\n                                      Result: Draw\n";
-            colorDefault();
-        }
-        else if (result == 1){
-            colorGreen();
-            cout<< "\n                                      Result: You win!\n";
-            colorDefault();
-        }
-        else{
-            colorRed();
-            cout<< "\n                                      Result: Computer wins!\n";
-            colorDefault();
-        }
-
-        cout<< "\n\n                         Press Y to exit, press any key to continue.";
-        exitKey = tolower(getch());
+        cout << "  +------------------------------------------+\n";
+        if (result == 0) { colorYellow(); cout << "  |          Result: Draw                    |\n"; }
+        else if (result == 1) { colorGreen(); cout << "  |          Result: You win!               |\n"; }
+        else { colorRed(); cout << "  |          Result: Computer wins!          |\n"; }
+        colorDefault();
+        cout << "  +------------------------------------------+\n";
+        cout << "  |   Press Y to exit, any key to continue.  |\n";
+        cout << "  +------------------------------------------+\n";
+        exitKey = tolower(_getch());
         system("cls");
-    }while(exitKey != 'y');
-
+    } while (exitKey != 'y');
 }
 
 //text color
@@ -265,77 +250,70 @@ void colorYellow() {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
 }
 
-void colorOrange() {
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
-}
-
 void colorDefault() {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 }
 
-//countdown
-void countdown(){
-    colorRed();cout<< "\n\n                             __";Sleep(200);cout<< "__";Sleep(200);cout<<3;Sleep(200);cout<< "__";Sleep(200);
-    cout<< "__";Sleep(200);colorYellow();cout<< "__";Sleep(200);cout<<2;Sleep(200);cout<< "__";Sleep(200);
-    cout<< "__";Sleep(200);colorGreen();cout<< "__";Sleep(200);cout<<1;Sleep(200);cout<< "___***___**__*_";Sleep(250);colorDefault();
+void countdown() {
+    cout<< "  +-------------------------+\n";
+    colorRed();   cout << "  [3]..."; Sleep(200);
+    colorYellow(); cout << " [2]..."; Sleep(200);
+    colorGreen();  cout << " [1]...";
+    cout<< " GO!\n"; Sleep(250);
+    colorDefault();
+    cout<< "  +-------------------------+\n";
 }
 
-void computerThinking(){
-    cout<<endl;
-    for(int i = 0; i <7 ; i++ ){
-        cout<< "                        __... loading...\n";
-        Sleep(200);
-        cout << "\33[0A\33[0K\r";
-    }
+void computerThinking() {
+    colorBlue();
+    cout << "\n  +------------------------------------------+\n  | Computer thinking";
+    for (int i = 0; i < 5; i++) { cout << "."; Sleep(200); }
+    cout<< " Done!           |\n";
+    cout<< " +------------------------------------------+\n";
+    colorDefault();
 }
 
-//icons
 void printTwoIcons(int a, int b) {
-
     string icon1[6], icon2[6];
-
-    // player1's icon
     if (a == 1) {
-        icon1[0] = "  _________       ";
-        icon1[1] = "  //00O0O00O\\     ";
-        icon1[2] = "  //09O00O09O0\\\   ";
-        icon1[3] = " \\9080O600O0O//   ";
-        icon1[4] = " \\9080OO809//     ";
-        icon1[5] = "   \\9080OO80/     ";
+        icon1[0] = "  _________  ";
+        icon1[1] = "  //00O0O00O\\ ";
+        icon1[2] = "  //09O00O09O0\\\\";
+        icon1[3] = " \\9080O600O0O// ";
+        icon1[4] = " \\9080OO809//  ";
+        icon1[5] = "   \\9080OO80/  ";
     }
     else if (a == 2) {
-        icon1[0] = " __     __       ";
-        icon1[1] = " ||     ||       ";
-        icon1[2] = " |(@@@@)||       ";
-        icon1[3] = " (@@@@@@@)       ";
-        icon1[4] = "(@@@@@@@@@)      ";
-        icon1[5] = " (@@@@@@@)       ";
+        icon1[0] = " __   __   ";
+        icon1[1] = " ||   ||   ";
+        icon1[2] = " |(@@)||   ";
+        icon1[3] = " (@@@@@@)  ";
+        icon1[4] = "(@@@@@@@)  ";
+        icon1[5] = " (@@@@@@)  ";
     }
     else if (a == 3) {
-        icon1[0] = "(\\   /)         ";
-        icon1[1] = " (\\ /)          ";
-        icon1[2] = "  (X)           ";
-        icon1[3] = "  /_\\           ";
-        icon1[4] = " /| |\\          ";
-        icon1[5] = "/_| |_\\         ";
+        icon1[0] = "(\\   /)   ";
+        icon1[1] = " (\\ /)    ";
+        icon1[2] = "  (X)     ";
+        icon1[3] = "  /_\\     ";
+        icon1[4] = " /| |\\    ";
+        icon1[5] = "/_| |_\\   ";
     }
-
-    // player2's icon
     if (b == 1) {
-        icon2[0] = "   _________ ";
-        icon2[1] = "  //00O0O00O\\ ";
-        icon2[2] = " //09O00O09O0\\\\ ";
-        icon2[3] = "  \\9080O600O0O// ";
+        icon2[0] = "   _________";
+        icon2[1] = "  //00O0O00O\\";
+        icon2[2] = " //09O00O09O0\\\\";
+        icon2[3] = "  \\9080O600O0O//";
         icon2[4] = "   \\9080OO809//";
-        icon2[5] = "    \\9080OO80/ ";
+        icon2[5] = "    \\9080OO80/";
     }
     else if (b == 2) {
-        icon2[0] = "  __      __";
-        icon2[1] = "  ||      ||";
-        icon2[2] = "  ||(@@@@)||";
-        icon2[3] = "  (@@@@@@@)";
-        icon2[4] = " (@@@@@@@@@)";
-        icon2[5] = "  (@@@@@@@)";
+        icon2[0] = "  __   __";
+        icon2[1] = "  ||   ||";
+        icon2[2] = "  ||(@@)||";
+        icon2[3] = "  (@@@@@@)";
+        icon2[4] = " (@@@@@@@)";
+        icon2[5] = "  (@@@@@@)";
     }
     else if (b == 3) {
         icon2[0] = "  (\\   /)";
@@ -345,8 +323,6 @@ void printTwoIcons(int a, int b) {
         icon2[4] = "   /| |\\";
         icon2[5] = "  /_| |_\\";
     }
-
-    for (int i = 0; i < 6; i++) {
-        cout<< "                        " << icon1[i] << "                 " << icon2[i] << endl;
-    }
+    for (int i = 0; i < 6; i++)
+        cout << "  " << icon1[i] << "   " << icon2[i] << endl;
 }
